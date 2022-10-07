da9e87322fcc55fb1e8c9a1dc8baa115ab20043d
EvaBenesova <ebenesova112@seznam.cz>
2022-10-07 13:27:54 +0200

    teambuilding added

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,9 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navbar__menu");
+const main = document.querySelector(".main");
+
 
 hamburger.addEventListener("click", () => {
    hamburger.classList.toggle("active");
    navMenu.classList.toggle("active");
+   main.classList.toggle("main-move");
 })
 
 
@@ -11,3 +14,5 @@ document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click",
    hamburger.classList.remove("active");
    navMenu.classList.remove("active");
  }))
+
+
